36382f1d0372f6594b11bd1303778c4542e11d4e
gerardo wemans <gerardo.wemans@gmail.com>
2022-11-18 16:31:30 +0000

    added root path

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   # get 'lists/show'
   # get 'lists/create'
   # get 'lists/edit'
+  root 'lists#index'
   resources :lists, only: [:index, :new, :show, :create, :destroy] do
     resources :bookmarks, only: [:new, :create]
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -14,5 +14,8 @@ Movie.create(title: "Titanic", overview: "101-year-old Rose DeWitt Bukater tells
 Movie.create(title: "Ocean's Eight", overview: "Debbie Ocean, a criminal mastermind, gathers a crew of female thieves to pull off the heist of the century.", poster_url: "https://image.tmdb.org/t/p/original/MvYpKlpFukTivnlBhizGbkAe3v.jpg", rating: 7.0)
 
 30.times do
-  Movie.create(title: Faker::Movie.title, rating: rand(0.0..10.0))
+  Movie.create(title: Faker::Movie.title, overview: "Debbie Ocean, a criminal mastermind, gathers a crew of female thieves to pull off the heist of the century.", poster_url: "https://image.tmdb.org/t/p/original/MvYpKlpFukTivnlBhizGbkAe3v.jpg", rating: rand(0.0..10.0))
+
 end
+
+puts "created"
